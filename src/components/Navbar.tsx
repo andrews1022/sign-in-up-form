bdde5fcf05dd3f7aa417c18d1a16a26b25c04037
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-16 10:01:49 -0700

    added button from shadcn ui and updated navbar to use it

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,20 @@
 import Link from "next/link";
 import { HandMetal } from "lucide-react";
+import { buttonVariants } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <nav className=" bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
-      <ul className="container flex items-center justify-between">
-        <li>
-          <Link href="/">
-            <HandMetal />
-          </Link>
-        </li>
+    <div className=" bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <HandMetal />
+        </Link>
 
-        <li>
-          <Link href="/sign-in">Sign in</Link>
-        </li>
-      </ul>
-    </nav>
+        <Link className={buttonVariants()} href="/sign-in">
+          Sign in
+        </Link>
+      </div>
+    </div>
   );
 };
 
