fda95327764625e22150fe1acc9f9c5f4d0a3e37
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-19 15:12:13 -0700

    updated authorize function inside CredentialsProvider

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,8 +1,9 @@
 import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import type { AuthOptions } from "next-auth";
 
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
+import { compare } from "bcrypt";
 
 export const options: AuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -13,23 +14,39 @@ export const options: AuthOptions = {
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
-        const res = await fetch("/your/endpoint", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" }
-        });
-
-        const user = await res.json();
-
-        if (res.ok && user) {
-          return user;
+        // check if the email or password is empty
+        if (!credentials?.email || !credentials?.password) {
+          return null;
         }
 
-        return null;
+        // check if email exists in db
+        const existingUser = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email
+          }
+        });
+
+        if (!existingUser) {
+          return null;
+        }
+
+        // check if the password doesn't match
+        // remember we use bcrypt to encrypt the password
+        const passwordMatch = await compare(credentials?.password, existingUser.password);
+
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return {
+          id: `${existingUser.id}`, // string expected, but id is a number
+          username: existingUser.username,
+          email: existingUser.email
+        };
       }
     })
   ],
